5467c912cbd949df3bff20b000de5357939c87de
Benjamin <panki0xff@gmail.com>
2021-06-17 23:14:06 +0800

    Created Dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription, timer } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+interface SEC_DATA {
+  account_executive: number;
+  daily_turnover: number;
+  accumulated_turnover: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  subscription: Subscription = new Subscription;
+  fiveSecondTimer: Observable<number> = timer(0, 5000);
   title = 'hsbc-angular-test';
+  page = 1;
+  pageSize = 10;
+  collectionSize = 0;
+  sort = '';
+  sortDirection = 'ASC';
+  secData: SEC_DATA[] = [];
+
+  constructor(private http: HttpClient) {
+    this.http = http;
+    this.firstLodd();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+  ngOnInit(): void {
+    this.subscription = this.fiveSecondTimer.subscribe(() => {
+      this.refreshData();
+    })
+  }
+
+  firstLodd() {
+    console.debug("firstload");
+    this.http.get<SEC_DATA[]>("http://rtq.chicheongweng.com:3000/securities").subscribe(r => {
+      this.collectionSize = r.length;
+    })
+  }
+
+  onSort(event: any) {
+    console.debug(event)
+  }
+  clickSort(name: string) {
+    console.debug(name)
+    if (name != this.sort) {
+      this.sort = name;
+      this.sortDirection = 'ASC';
+    } else {
+      if (this.sortDirection == 'DESC') {
+        this.sortDirection = 'ASC'
+      } else {
+        this.sortDirection = 'DESC'
+      }
+    }
+    this.refreshData()
+  }
+
+  refreshData() {
+    let url = "http://rtq.chicheongweng.com:3000/securities?";
+    url = url + "_start=" + ((this.page - 1) * this.pageSize);
+    url = url + "&_limit=" + (this.pageSize);
+    if (this.sort != '') {
+      url = url + "&_sort=" + this.sort + "&_order=" + this.sortDirection + ""
+    }
+    console.debug("refresh " + url);
+    this.http.get<SEC_DATA[]>(url).subscribe(r => {
+      this.secData = r;
+      console.debug(r);
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { BootstrapIconsModule } from 'ng-bootstrap-icons';
+import { allIcons} from 'ng-bootstrap-icons/icons';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule,
+    BootstrapIconsModule.pick(allIcons)
+  ],
+  exports: [
+    BootstrapIconsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
